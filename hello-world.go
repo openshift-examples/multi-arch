86426a8e3ce34c46f00ce167565525d683b31269
Robert Bohne <robert.bohne@redhat.com>
2020-10-27 10:10:18 +0100

    Update hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -1,5 +1,14 @@
-package main
+import "os/exec"
 import "fmt"
+
 func main() {
-    fmt.Println("hello world")
+    cmd := exec.Command("uname", "-m")
+    stdout, err := cmd.Output()
+
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+    fmt.Print("hello world from architecure: ")
+    fmt.Print(string(stdout))
 }
